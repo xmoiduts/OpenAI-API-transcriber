773727eb7a497a0a4bdf32080a10595a228a8178
xmoiduts <xmoiduts@users.noreply.github.com>
2024-11-03 01:29:29 +0800

    cleanup: redundant code

--- src/gui/time_slicer_tab.py
+++ src/gui/time_slicer_tab.py
@@ -166,56 +166,6 @@ class SegmentBar(QFrame):
         super().leaveEvent(event)
 """
 
-# class FlyingLabel(QLabel):
-#     def __init__(self, parent=None):
-#         super().__init__(parent)
-#         self.setAlignment(Qt.AlignCenter)
-#         self.setStyleSheet("""
-#             background-color: rgba(0, 0, 0, 0);
-#             color: rgba(255, 255, 255, 0);
-#             border-radius: 10px;
-#             padding: 5px;
-#         """)
-#         self.setWordWrap(True)
-#         self.timer = QTimer(self)
-#         self.timer.timeout.connect(self.animate)
-#         self.opacity = 0.0
-#         self.y_position = 0
-#         self.animation_state = "fly_in"
-
-#     def animate(self):
-#         if self.animation_state == "fly_in":
-#             self.opacity = min(1.0, self.opacity + 0.1)
-#             self.y_position = max(self.target_y, self.y_position - 2)
-#             self.move(self.x(), int(self.y_position))
-#             self.update_style()
-            
-#             if self.opacity >= 1.0 and self.y_position <= self.target_y:
-#                 self.animation_state = "stay"
-#                 self.timer.stop()
-#                 self.stay_timer.start()
-        
-#         elif self.animation_state == "fade_out":
-#             self.opacity = max(0.0, self.opacity - 0.1)
-#             self.update_style()
-            
-#             if self.opacity <= 0:
-#                 self.hide()
-#                 self.timer.stop()
-
-#     def update_style(self):
-#         self.setStyleSheet(f"""
-#             background-color: rgba(0, 0, 0, {int(180 * self.opacity)});
-#             color: rgba(255, 255, 255, {int(255 * self.opacity)});
-#             border-radius: 10px;
-#             padding: 5px;
-#         """)
-
-#     def start_fade_out(self):
-#         self.animation_state = "fade_out"
-#         self.timer.start(16)
-# 
-
 class TimeSlicerTab(TabInterface):
     def __init__(self):
         super().__init__("Time Slicer")
